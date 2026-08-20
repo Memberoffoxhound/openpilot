918b441773cca8eb982e1a274b84e0cce10faf37
Memberoffoxhound <70163566+Memberoffoxhound@users.noreply.github.com>
2026-08-20 16:42:56 +0000

    Recover HLS on return, cut lag, map+dcam row, full-width trip

--- openpilot/system/webrtc/hls.py
+++ openpilot/system/webrtc/hls.py
@@ -203,7 +203,7 @@ class _CamBuf:
       "#EXT-X-VERSION:3",
       "#EXT-X-INDEPENDENT-SEGMENTS",
       "#EXT-X-TARGETDURATION:1",
-      "#EXT-X-START:TIME-OFFSET=-0.4,PRECISE=YES",
+      "#EXT-X-START:TIME-OFFSET=-0.25,PRECISE=YES",
       f"#EXT-X-MEDIA-SEQUENCE:{segs[0][0]}",
     ]
     for seq, dur, _ in segs:
