f785ef102b3813f10ea469abeac24d98a6e6dce3
Memberoffoxhound <70163566+Memberoffoxhound@users.noreply.github.com>
2026-09-05 19:49:18 -0700

    webrtcd: tap encoderd like Delamain, rebind on onroad/offroad

--- openpilot/system/webrtc/device/video.py
+++ openpilot/system/webrtc/device/video.py
@@ -20,6 +20,21 @@ TIMING_SEI_UUID = bytes([
 ])
 _SEI_PREFIX = b'\x00\x00\x00\x01\x06\x05\x30' + TIMING_SEI_UUID
 
+# Prefer the producer that is already running. Onroad that is encoderd
+# (qcam H264 + HEVC mains). Offroad that is stream_encoderd livestream*.
+# Never start a second encoder just to feed WebRTC.
+_ONROAD_SOCKS = {
+  "driver": ("cabinEncodeData", "driverEncodeData", "livestreamCabinEncodeData"),
+  "wideRoad": ("wideRoadEncodeData", "livestreamWideRoadEncodeData"),
+  "road": ("qNarrowRoadEncodeData", "roadEncodeData", "livestreamNarrowRoadEncodeData"),
+}
+_OFFROAD_SOCKS = {
+  "driver": ("livestreamCabinEncodeData", "cabinEncodeData", "driverEncodeData"),
+  "wideRoad": ("livestreamWideRoadEncodeData", "wideRoadEncodeData"),
+  "road": ("livestreamNarrowRoadEncodeData", "qNarrowRoadEncodeData", "roadEncodeData"),
+}
+_STALE_S = 1.0
+
 
 @dataclass(frozen=True)
 class EncodedVideoFrame:
@@ -31,44 +46,59 @@ class EncodedVideoFrame:
 
 
 class LiveStreamVideoStreamTrack(TiciVideoStreamTrack):
-  # stream_encoderd (offroad / notcar)
-  live_socks = {
-    "driver": "livestreamCabinEncodeData",
-    "wideRoad": "livestreamWideRoadEncodeData",
-    "road": "livestreamNarrowRoadEncodeData",
-  }
-  # onroad encoderd. qcam is already H264; road/wide/cabin mains are HEVC.
-  onroad_socks = {
-    "driver": "cabinEncodeData",
-    "wideRoad": "wideRoadEncodeData",
-    "road": "qNarrowRoadEncodeData",
-  }
-
   def __init__(self, camera_type: str, video_enabled: bool = True):
     super().__init__(camera_type, DT_MDL)
 
-    self._sock = self._make_sock(camera_type)
+    self._camera_type = camera_type
     self._pts = 0
     self._t0_ns = time.monotonic_ns()
     self.timing_sei_enabled = False
     self.params = Params()
     self._seen_keyframe = False
     self.video_enabled = video_enabled
+    self._offroad = self.params.get_bool("IsOffroad")
+    self._candidates = self._candidate_names(camera_type, self._offroad)
+    self._cand_i = 0
+    self._sock_name = self._candidates[0]
+    self._sock = messaging.sub_sock(self._sock_name, conflate=True)
+    self._last_frame_mono = time.monotonic()
 
   def stop(self) -> None:
     super().stop()
     self._sock = None
 
-  def _sock_name(self, camera_type: str) -> str:
-    if self.params.get_bool("IsOffroad"):
-      return self.live_socks[camera_type]
-    return self.onroad_socks[camera_type]
+  def _candidate_names(self, camera_type: str, offroad: bool) -> tuple[str, ...]:
+    table = _OFFROAD_SOCKS if offroad else _ONROAD_SOCKS
+    return table[camera_type]
 
-  def _make_sock(self, camera_type: str) -> messaging.SubSocket:
-    return messaging.sub_sock(self._sock_name(camera_type), conflate=True)
+  def _bind(self, name: str) -> None:
+    self._sock_name = name
+    self._sock = messaging.sub_sock(name, conflate=True)
+    self._seen_keyframe = False
+    self._last_frame_mono = time.monotonic()
+
+  def _resync_producer(self) -> None:
+    offroad = self.params.get_bool("IsOffroad")
+    names = self._candidate_names(self._camera_type, offroad)
+    if offroad != self._offroad or names != self._candidates:
+      self._offroad = offroad
+      self._candidates = names
+      self._cand_i = 0
+      self._bind(names[0])
+      return
+    if (time.monotonic() - self._last_frame_mono) < _STALE_S:
+      return
+    if len(self._candidates) < 2:
+      return
+    self._cand_i = (self._cand_i + 1) % len(self._candidates)
+    self._bind(self._candidates[self._cand_i])
 
   def switch_camera(self, camera_type: str) -> None:
-    self._sock = self._make_sock(camera_type)
+    self._camera_type = camera_type
+    self._offroad = self.params.get_bool("IsOffroad")
+    self._candidates = self._candidate_names(camera_type, self._offroad)
+    self._cand_i = 0
+    self._bind(self._candidates[0])
 
   def enable(self, enabled: bool):
     self.video_enabled = enabled
@@ -94,20 +124,21 @@ class LiveStreamVideoStreamTrack(TiciVideoStreamTrack):
 
   async def recv(self):
     while True:
-      # while video is disabled, pause here without returning
       if not self.video_enabled:
         await asyncio.sleep(0.005)
         continue
 
-      msg = messaging.recv_one_or_none(self._sock)
+      self._resync_producer()
+      msg = messaging.recv_one_or_none(self._sock) if self._sock is not None else None
       if msg is not None:
         if not self._seen_keyframe and (getattr(msg, msg.which()).idx.flags & V4L2_BUF_FLAG_KEYFRAME):
           self._seen_keyframe = True
           self.params.put("LivestreamRequestKeyframe", False, block=False)
+        self._last_frame_mono = time.monotonic()
         break
       await asyncio.sleep(0.005)
 
-    self._pts =  ((time.monotonic_ns() - self._t0_ns) * self._clock_rate) // 1_000_000_000
-    self.log_debug("track sending frame %d", self._pts)
+    self._pts = ((time.monotonic_ns() - self._t0_ns) * self._clock_rate) // 1_000_000_000
+    self.log_debug("track sending frame %d from %s", self._pts, self._sock_name)
 
     return EncodedVideoFrame(self._build_frame_data(msg), self._pts)
